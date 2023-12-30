75960b3ee3b466565cf2631575976029f8f7395c
Yash-9558 <20bce076@nirmauni.ac.in>
2023-12-30 15:05:40 +0530

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact";
 
 const AppLayout = () => {
   return (
@@ -11,5 +15,25 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  // {
+  //   path: "*",
+  //   element: <Error />,
+  // },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
